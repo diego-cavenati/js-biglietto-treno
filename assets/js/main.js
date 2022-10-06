a288e0251540dac7589f4031005dfaae1adbc2f8
diego-cavenati <diegocavenati@gmail.com>
2022-10-06 14:01:58 +0200

    Calculate ticket price - action plan

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*************************
+ calculate ticket price
+**************************/
+
+// 1 - ask the user the number of kilometres he/she wants to travel
+
+// 2 - ask the user age
+
+// 3 - calculate the total price of the trip:
+// ticket price is defined according to km (0.21 € per km)
+
+// 20% discount for minors
+
+// 40% discount for the over 65s
+
+// The output of the final price must have a maximum of two decimal places, to indicate cents on the price
