c2fa33700b5529ed8a1c1ed5b24cff99affb52f0
diego-cavenati <diegocavenati@gmail.com>
2022-10-06 14:25:21 +0200

    price without discount and the variables discounts

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,15 @@ const userAge = Number(prompt("How old are you?"));
 
 // 3 - calculate the total price of the trip:
 // ticket price is defined according to km (0.21 € per km)
+const priceKm = 0.21;
+
+// ticket price without discounts
+const ticketFullPrice = userKm * priceKm;
 
 // 20% discount for minors
+const discountMinors = (ticketFullPrice / 100) * 20;
 
 // 40% discount for the over 65s
+const discountOver = (ticketFullPrice / 100) * 40;
 
 // The output of the final price must have a maximum of two decimal places, to indicate cents on the price
